c04bd89ab1d0e891668f52a50175691ea4f38df4
SamirIASchool <94679124+SamirIASchool@users.noreply.github.com>
2022-03-09 14:15:58 +0100

    uncoment main

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import function
 
 
-# function.identite()
-# function.age()
-# function.cone()
-# function.ispair()
-# function.fibonacci()
-# function.pgcd_ppcm()
+function.identite()
+function.age()
+function.cone()
+function.ispair()
+function.fibonacci()
+function.pgcd_ppcm()
 
-# param1 = [1,2,3,4,5,6,7,8,9]
-# param2 = [1,3,5,7,12,8,8,9,1]
+param1 = [1,2,3,4,5,6,7,8,9]
+param2 = [1,3,5,7,12,8,8,9,1]
 
-# liste_commun = function.element_communs(param1, param2)
-# print(liste_commun)
+liste_commun = function.element_communs(param1, param2)
+print(liste_commun)
 
-# function.Palindrome()
-# liste = [1,2,5,4,8,6,1,2,7,8,5,2]
-# resultaListe = function.doublon(liste)
+function.Palindrome()
+liste = [1,2,5,4,8,6,1,2,7,8,5,2]
+resultaListe = function.doublon(liste)
 
 
